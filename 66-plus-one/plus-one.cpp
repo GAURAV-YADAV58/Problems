bf7c43334c45ce97eeb44397003856d199fc3cd3
GAURAV YADAV <yadavgaurav1p0@gmail.com>
2025-12-18 01:48:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (14.54%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0; i--){
+            if(digits.at(i) < 9){
+                digits.at(i)++;
+                return digits;
+            }
+            digits.at(i) = 0;
+        }
+        digits.insert(digits.begin() , 1);
+        return digits;  
+    }
+};

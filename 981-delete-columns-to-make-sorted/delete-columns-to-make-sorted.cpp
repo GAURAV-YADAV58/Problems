f641c54bface59ff3d000dfa3a1ac125a763eb64
GAURAV YADAV <yadavgaurav1p0@gmail.com>
2025-12-20 19:01:57 +0530

    Time: 4 ms (45.13%) | Memory: 15.6 MB (14.42%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int del = 0;
+        int rows = strs.size();
+        int cols = strs[0].size();
+        for(int i = 0; i < cols; i++){
+            for(int j = 0; j < rows-1; j++){
+                if(strs[j][i] > strs[j+1][i]){
+                    del++;
+                    break;
+                }
+            }
+        }
+        return del;
+    }
+};

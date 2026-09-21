46ffb1f1ad22c2eede8f9a08c3d1e17fdc00e01d
GAURAV YADAV <yadavgaurav1p0@gmail.com>
2026-09-22 00:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (84.65%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void getSubsets(vector<int>& nums,vector<int>& ans, int i, vector<vector<int>>& subsets){
-        int n = nums.size();
+    void getSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets){
         if(i == nums.size()){
-            subsets.push_back(ans);
+            allSubsets.push_back(ans);
             return;
         }
         ans.push_back(nums[i]);
-        getSubsets(nums, ans, i + 1, subsets);
+        getSubsets(nums, ans, i + 1, allSubsets);
         ans.pop_back();
         int idx = i + 1;
-        while(idx < n && nums[idx] == nums[idx - 1]){
+        while(idx < nums.size() && nums[idx] == nums[idx - 1]){
             idx++;
         }
-        getSubsets(nums,ans, idx, subsets);
+        getSubsets(nums, ans, idx, allSubsets);
     }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> subsets;
         vector<int> ans;
-        getSubsets(nums, ans, 0, subsets);
-        return subsets;
+        vector<vector<int>> allSubsets;
+        getSubsets(nums, ans, 0, allSubsets);
+        return allSubsets;    
     }
 };

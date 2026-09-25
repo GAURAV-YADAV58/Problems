bdf067e42a830cf532f5606f8e387acda78dd558
GAURAV YADAV <yadavgaurav1p0@gmail.com>
2026-09-26 01:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (88.33%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans){
         if(idx == nums.size()){
-            ans.push_back({nums});
+            ans.push_back(nums);
             return;
         }
         for(int i = idx; i < nums.size(); i++){
-            swap(nums[idx], nums[i]);
+            swap(nums[i], nums[idx]);
             getPerms(nums, idx + 1, ans);
-            swap(nums[idx],nums[i]);
+            swap(nums[i], nums[idx]);
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         getPerms(nums, 0, ans);
